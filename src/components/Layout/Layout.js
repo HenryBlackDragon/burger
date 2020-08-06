3fc6209aac8eb9a7dd9d35fb26a32827ae8f6607
Alexander Chaban <alex-chaban@mail.ru>
2020-08-06 15:32:02 +0300

    add hamberger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,16 +7,22 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHamdler = () => {
+        this.setState((prevState) => {
+            return ({ showSideDrawer: !prevState.showSideDrawer });
+        });
+    }
+
     render() {
         return (<Aux>
-            <Toolbar />
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHamdler} />
             <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
             <main className={classes.Content}>
                 {this.props.children}
