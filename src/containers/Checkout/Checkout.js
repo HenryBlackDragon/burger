cb0b276b0462a82ab2ffcc30235f19de647812e8
Alexander Chaban <alex-chaban@mail.ru>
2020-08-21 12:29:50 +0300

    passing ingredients via query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,15 +3,32 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
-
     state = {
         ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
+            salad: 1
         }
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+
+        this.setState({ingredients: ingredients});
+    }
+
+    // componentDidMount() {
+    //     const query = new URLSearchParams(this.props.location.search);
+    //     const ingredients = {};
+    //     for (let param of query.entries()) {
+    //         // ['salad', '1']
+    //         ingredients[param[0]] = +param[1];
+    //     }
+    //     this.setState({ingredients: ingredients});
+    // }
 
 
     checkoutCancelledHandler = () => {
