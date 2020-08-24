e5f56e2e5d63ed8a398e995005461cea7da997b2
Alexander Chaban <alex-chaban@mail.ru>
2020-08-24 11:38:12 +0300

    nav to contact data

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -17,19 +19,8 @@ class Checkout extends Component {
             ingredients[param[0]] = +param[1];
         }
 
-        this.setState({ingredients: ingredients});
+        this.setState({ ingredients: ingredients });
     }
-
-    // componentDidMount() {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients = {};
-    //     for (let param of query.entries()) {
-    //         // ['salad', '1']
-    //         ingredients[param[0]] = +param[1];
-    //     }
-    //     this.setState({ingredients: ingredients});
-    // }
-
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -45,8 +36,8 @@ class Checkout extends Component {
                 <CheckoutSummary
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                />
+                    checkoutContinued={this.checkoutContinuedHandler} />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         );
     }
