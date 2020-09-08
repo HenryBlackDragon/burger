ef9b7f52c9fd03fc37d4df4c7b26b23dfe053767
Alexander Chaban <alex-chaban@mail.ru>
2020-09-08 18:33:48 +0300

    update purchase method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionType from '../../store/actions';
 class BurgerBuilder extends Component {
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false
     };
@@ -34,7 +33,8 @@ class BurgerBuilder extends Component {
             }).reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({ purchasable: sum > 0 });
+
+        return sum > 0;
     };
 
     purchaseHandler = () => {
@@ -82,7 +82,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={() => this.props.onIgredientRemoved()}
                     disabled={disabledInfo}
                     price={this.props.price}
-                    purchasable={this.state.purchasable}
+                    purchasable={this.updatePurchaseState(this.props.ings)}
                     ordered={this.purchaseHandler}
                 />
             </Aux>);
