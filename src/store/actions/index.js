13964fe34b26b55babd9b258072d1690da62a391
Alexander Chaban <alex-chaban@mail.ru>
2020-10-13 16:22:12 +0300

    add logout link

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
 export { purchaseBurger, purcahseInit, fetchOrders } from './order';
-export { auth } from './auth';
+export { auth, logout } from './auth';
