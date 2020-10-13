4ca1cf37c9e7a91dd57aadbb365e95b8f6077eb8
Alexander Chaban <alex-chaban@mail.ru>
2020-10-13 13:55:27 +0300

    logging users

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -40,12 +54,13 @@ export const auth = (email, password, isSignup) => {
         }
 
         axios.post(url, authData)
-            .then(repsonse => {
-                console.log(repsonse);
-                dispatch(authSuccess(repsonse.data.idToken, repsonse.data.localId));
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
-                dispatch(authFail(err.repsonse.data.error));
+                dispatch(authFail(err.response.data.error));
             });
     };
 };
