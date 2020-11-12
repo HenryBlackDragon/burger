6a63f1366da523606ee4c5552951747616e860ed
Alexander Chaban <alex-chaban@mail.ru>
2020-11-12 18:23:09 +0300

    test container

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,23 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
     it('should render two <NavigationItems /> elements if not auth', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
-    })
+    });
+
+    it('should render three <NavigationItems /> elements if auth', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render three <NavigationItems /> elements if auth', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
