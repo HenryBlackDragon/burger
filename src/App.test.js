79aa7c38979d69861235fbad473fbeb260c05a0f
Alexander Chaban <alex-chaban@mail.ru>
2020-11-21 00:48:09 +0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
+  const x = "sdad";
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
